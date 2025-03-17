4203e6c8de04dbe364cab013f7a5e5377edef415
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-03-17 11:13:03 +0530

    arr2.cpp
    C++ program to print the array elements of even indices.

---
+++ arr2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of array:"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"Array elements:";
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+        cout<<a[i]<<endl;
+    }
+    }
+    return 0;
+}
