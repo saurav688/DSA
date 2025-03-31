a56b406f221d8ab0209393f68b0d371a1a163da6
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-03-31 11:16:24 +0530

    calculator1.cpp
    C++ program for creating a simple calculator.

---
+++ calculator1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter 1st no.:"<<endl;
+    cin>>x;
+    cout<<"Enter 2nd no.:"<<endl;
+    cin>>y;
+    cout<<"Sum: "<<x+y<<endl;
+    cout<<"Difference: "<<y-x<<endl;
+    cout<<"Product: "<<x*y<<endl;
+    cout<<"Quotient: "<<y/x<<endl;
+    return 0;
+}
