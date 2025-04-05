31aa7c864b9cee40b1007bb2867f479379d6135a
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-04-05 14:06:30 +0530

    2Darr3.cpp
    C++ program for matrix transpose.

---
+++ 2Darr3.cpp
@@ -0,0 +1,33 @@
+//matrix transpose
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter the size of array:";
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+    cout<<endl;
+    }
+    int trans[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            trans[i][j]=arr[j][i];
+       }
+    }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<trans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
