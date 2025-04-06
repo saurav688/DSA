e4a78fd938cdbabac866c3fb5d2968d0badfcc39
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-04-06 18:18:07 +0530

    vector1.cpp
    C++ program to find the size and capacity of vector.

---
+++ vector1.cpp
@@ -0,0 +1,25 @@
+//size and capacity of vector
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v;
+    cout<<"Size:"<<v.size()<<endl;
+    cout<<"Capacity:"<<v.capacity()<<endl;
+    v.push_back(1);
+    cout<<"Size:"<<v.size()<<endl;
+    cout<<"Capacity:"<<v.capacity()<<endl;
+     v.push_back(2);
+    cout<<"Size:"<<v.size()<<endl;
+    cout<<"Capacity:"<<v.capacity()<<endl; 
+    v.push_back(3);
+    cout<<"Size:"<<v.size()<<endl;
+    cout<<"Capacity:"<<v.capacity()<<endl;
+    v.resize(8);
+    cout<<"Size:"<<v.size()<<endl;
+    cout<<"Capacity:"<<v.capacity()<<endl;
+     v.resize(10);
+    cout<<"Size:"<<v.size()<<endl;
+    cout<<"Capacity:"<<v.capacity()<<endl;
+    return 0;
+}
