3e58994a0be16123648036f37ab2d47e463f8053
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-04-13 22:13:07 +0530

    vector8.cpp
    C++ program for checking queries.

---
+++ vector8.cpp
@@ -0,0 +1,26 @@
+//check queries
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    const int N=1e5+10;
+    vector<int>freq(N,0);
+    for(int i=0;i<n;i++){
+        freq[v[i]]++;
+    }
+    cout<<"Enter queries:";
+    int q;
+    cin>>q;
+    while(q--){
+        int queryEle;
+        cin>>queryEle;
+        cout<<freq[queryEle]<<endl;
+    }
+    return 0;
+}
