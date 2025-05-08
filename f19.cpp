d3ce731b92e30564180cdeeb524e441154eefbfc
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-05-08 20:32:25 +0530

    Create f19.cpp

---
+++ f19.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+string name="Sourav";
+void fun(){
+    cout<<name<<endl;
+}
+int main(){
+    cout<<name<<endl;
+    name="Tiwari";
+    fun();
+    return 0;
+}
