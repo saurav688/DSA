b44e222e2fdc368389f3d05b80d826a1df76981d
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-05-15 00:45:01 +0530

    conditional1.cpp

---
+++ conditional1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter the no.:"<<endl;
+    cin>>x;
+    if(x%2==0){
+        cout<<"The entered no. is even."<<endl;
+    }
+    else{
+        cout<<"The entered no. is odd."<<endl;
+    }
+    return 0;
+}
