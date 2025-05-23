6e32ea4d3838d931501ef225eda938b6c7d3a256
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-05-23 10:47:21 +0530

    factorial2.cpp

---
+++ factorial2.cpp
@@ -0,0 +1,16 @@
+//recursive method
+#include<iostream>
+using namespace std;
+int f(int n){
+    if(n==1){
+        return 1;
+    }
+    else{
+        return n*f(n-1);
+    }
+}
+int main(){
+    int result=f(5);
+    cout<<result;
+    return 0;
+}
