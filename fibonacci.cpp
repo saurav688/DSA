4fee1a5cf077d4560c551aec8e3d45ab25fb97fc
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-05-24 17:06:30 +0530

    fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fib(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+    else{
+        return fib(n-1)+fib(n-2);
+    }
+}
+int main(){
+    int result=fib(2);
+    cout<<result;
+    return 0;
+}
