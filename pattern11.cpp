d0a335617d89859b6fb653376521494638525094
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-06-13 22:47:39 +0530

    pattern11.cpp

--- pattern11.cpp
+++ pattern11.cpp
@@ -1,22 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,m;
+    int n;
     cout<<"Enter the row no.:";
     cin>>n;
-    cout<<"Enter column no.:";
-    cin>>m;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=(n-i);j++){
-            cout<<" ";
-        }
-        for(int j=1;j>=1;j=(i-1);j++){
-            cout<<" ";
-        }
-        for(int j=1;j<=m;j++){
-            cout<<j;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==n/2){
+                cout<<"*";
+            }
+            else if(j==n/2){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
         }
         cout<<endl;
     }
     return 0;
-}//doubt
+}
