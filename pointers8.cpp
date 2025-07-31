7b63b832ff9033f9510e4216ea3de2bfb309ff5b
Sourav_Tiwari <sourav688tiwari@gmail.com>
2025-07-31 21:08:20 +0530

    pointers8.cpp

---
+++ pointers8.cpp
@@ -0,0 +1,12 @@
+//Null pointer
+#include<iostream>
+using namespace std;
+int main(){
+    int *ptr=NULL;
+    int *p=0;
+    int *ptr2='\0';
+    cout<<ptr<<" "<<*ptr<<endl;
+    cout<<p<<" "<<*p<<endl;
+    cout<<ptr2<<" "<<*ptr2<<endl;
+    return 0;
+}
